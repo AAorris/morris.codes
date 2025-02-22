8df96020c23b99e8e5b32c76652223d0b943fd02
Aaron Morris <aaorris@gmail.com>
2025-02-21 21:55:36 -0500

    Add admin guard

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,6 +1,12 @@
 import { Nav } from "@/components/app/nav"
+import { adminFlag } from "@/flags";
+import { redirect } from "next/navigation";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
+  const isAdmin = await adminFlag();
+  if (!isAdmin) {
+    redirect("/404");
+  }
   return (
     <section id="admin" className="max-w-screen-lg mx-auto">
       <div className="p-[32px] flex flex-col gap-[16px]">
