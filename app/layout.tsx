868a5b2e6d173eeb00b2893b9b8374c504f73027
Aaron Morris <aaorris@gmail.com>
2025-02-21 21:52:53 -0500

    update default theme handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }>) {
   const themeCookie = (await cookies()).get('theme')?.value;
-  const theme = themeCookie === 'light' ? 'light' : 'dark';
+  const theme = themeCookie === 'dark' ? 'dark' : 'light';
   return (
     <html lang="en" className={theme}>
       <body
